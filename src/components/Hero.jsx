abbf27e7c3c12d6cc0389a6a39ad120f2d921026
Chanudi Neha <nehagimhani15@gmail.com>
2025-09-07 22:45:43 +0530

    Add new components, update App, delete Header, add assets

---
+++ src/components/Hero.jsx
@@ -0,0 +1,51 @@
+
+import profilePic from "../assets/chanudineha.jpg";
+
+const Hero = () => {
+  return (
+    <div className="pb-4 lg:mb-36">
+        <div className="flex flex-wrap lg:flex-row-reverse">
+            <div className="w-full lg:w-1/2">
+            <div className="flex justify-center lg:p-8">
+                 <img src={profilePic} alt="Chanudi Neha" className="w-64 h-64 object-cover border-stone-900 rounded-3xl" />
+
+            </div>
+
+            </div>
+ <div className="w-full lg:w-1/2">
+ <div className="flex flex-col items-center lg:items-start
+ mt-10"> 
+ <h2 className="pb-2 text-4xl tracking-tighter
+ lg:text-8xl">Chanudi Neha
+ </h2>
+ <span className="bg-gradient-to-r from-stone-300
+ to-stone-600 bg-clip-text text text-3xl tracking-tight
+ text-transparent">Full Stack Developer </span>
+<p className="my-2 max-w-lg py-6 text-xl leading-relaxed
+tracking-tighter">
+  I am a passionate developer who loves building modern web apps with React,
+  Tailwind, and backend technologies. Always learning and exploring new tools
+  to improve my skills.
+
+</p>
+<a href="/resume.pdf"
+target="_blank"
+rel="noopener noreferrer"
+ download
+    className="bg-white rounded-full p-4 text-sm
+    text-stone-800 mb-10">
+        Download Resume
+   
+
+</a>
+
+ </div>
+
+ </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Hero;
